14d168ffe3c06f98ddad6452a084b6150554a2d3
unifolia <james@electronicmail.ca>
2019-11-21 17:27:42 -0500

    :milky_way: Updates to header styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// API app idea --- an app that lets the user compare the weather in the city of their choosing with the weather on Mars. 
+// API app idea --- an app that lets the user compare the weather in the city of their choosing with the weather on Mars.
 
 // User will be greeted by a space-themed landing page with an input form. The instructions will tell the user to enter the name of their/any city and submit. On submit, two panels will slide into view with information on their selected city's weather and Mars' weather, respectively. Each panel will detail specifics about temperature, forecast, meteorological events etc. There will also be information on the page that directly compares the two e.g. Mars is ___ degrees colder than Toronto.
 
@@ -9,99 +9,100 @@
 const weatherCompare = {};
 
 const marsApi = `EwwwPaMx8qEpAqWR0mgPxqaVTDLBFsckriIbLtgc`;
-const weatherApi = `8f12648509075ad5f7b59b7ecc23f813`
+const weatherApi = `8f12648509075ad5f7b59b7ecc23f813`;
 
-weatherCompare.getMarsWeather = function () {
-    const marsWeatherResults = $.ajax({
-        url: `https://api.nasa.gov/insight_weather/?api_key=${marsApi}&feedtype=json&ver=1.0`,
-        method: `GET`,
-        dataType: `json`,
-        // version: 1,
-        // feedtype: `JSON`,
-        // api_key: `${marsApi}`
-    });
-    return marsWeatherResults;
-}
+weatherCompare.getMarsWeather = function() {
+  const marsWeatherResults = $.ajax({
+    url: `https://api.nasa.gov/insight_weather/?api_key=${marsApi}&feedtype=json&ver=1.0`,
+    method: `GET`,
+    dataType: `json`
+    // version: 1,
+    // feedtype: `JSON`,
+    // api_key: `${marsApi}`
+  });
+  return marsWeatherResults;
+};
 
-weatherCompare.addMarsData = function(marsResult){
-    const marsAvgTemperature = (marsResult[0]['343'].AT.av).toFixed(2);
-    const marsMaxTemperature = (marsResult[0]['343'].AT.mx).toFixed(2);
-    const marsMinTemperature = (marsResult[0]['343'].AT.mn).toFixed(2);
+weatherCompare.addMarsData = function(marsResult) {
+  const marsAvgTemperature = marsResult[0]["343"].AT.av.toFixed(2);
+  const marsMaxTemperature = marsResult[0]["343"].AT.mx.toFixed(2);
+  const marsMinTemperature = marsResult[0]["343"].AT.mn.toFixed(2);
 
-    let marsAvgSentence = (`It is ${marsAvgTemperature} degrees on Mars`);
-    let marsMaxSentence = (`There's a high of ${marsMaxTemperature}`);
-    let marsMinSentence = (`There's a low of ${marsMinTemperature}`);
+  let marsAvgSentence = `It is ${marsAvgTemperature} degrees on Mars`;
+  let marsMaxSentence = `There's a high of ${marsMaxTemperature}`;
+  let marsMinSentence = `There's a low of ${marsMinTemperature}`;
 
-    $('li.marsAverage').html(marsAvgSentence);
-    $('li.marsMax').html(marsMaxSentence);
-    $('li.marsMin').html(marsMinSentence);
-}
+  $("li.marsAverage").html(marsAvgSentence);
+  $("li.marsMax").html(marsMaxSentence);
+  $("li.marsMin").html(marsMinSentence);
+};
 
-weatherCompare.getCityWeather = function(userCity){
-    weatherCompare.getMarsWeather();
-    const cityWeatherResults = $.ajax({
-        url: `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${weatherApi}`,
-        method: `GET`,
-        dataType: `json`,
-    });
-    return cityWeatherResults;
-}
+weatherCompare.getCityWeather = function(userCity) {
+  weatherCompare.getMarsWeather();
+  const cityWeatherResults = $.ajax({
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${weatherApi}`,
+    method: `GET`,
+    dataType: `json`
+  });
+  return cityWeatherResults;
+};
 
-weatherCompare.addCityData = function(cityResult){
-    let cityName = cityResult[0].name;
+weatherCompare.addCityData = function(cityResult) {
+  let cityName = cityResult[0].name;
 
-    let cityAvgTemperature = (cityResult[0].main.temp - 273.15).toFixed(2);
-    let cityMaxTemperature = (cityResult[0].main.temp_max - 273.15).toFixed(2);
-    let cityMinTemperature = (cityResult[0].main.temp_min - 273.15).toFixed(2);
+  let cityAvgTemperature = (cityResult[0].main.temp - 273.15).toFixed(2);
+  let cityMaxTemperature = (cityResult[0].main.temp_max - 273.15).toFixed(2);
+  let cityMinTemperature = (cityResult[0].main.temp_min - 273.15).toFixed(2);
 
-    let cityAvgSentence = (`It is ${cityAvgTemperature} degrees in ${cityName}`);
-    let cityMaxSentence = (`There's a high of ${cityMaxTemperature}`);
-    let cityMinSentence = (`There's a high of ${cityMinTemperature}`);
+  let cityAvgSentence = `It is ${cityAvgTemperature} degrees in ${cityName}`;
+  let cityMaxSentence = `There's a high of ${cityMaxTemperature}`;
+  let cityMinSentence = `There's a high of ${cityMinTemperature}`;
 
-    $('li.cityAverage').html(cityAvgSentence);
-    $('li.cityMax').html(cityMaxSentence);
-    $('li.cityMin').html(cityMinSentence);
-}
+  $("li.cityAverage").html(cityAvgSentence);
+  $("li.cityMax").html(cityMaxSentence);
+  $("li.cityMin").html(cityMinSentence);
+};
 
-weatherCompare.addDifferenceData = function(marsResult, cityResult){
-    const cityAvgTemperature = (cityResult[0].main.temp - 273.15).toFixed(2);
-    const marsAvgTemperature = (marsResult[0]['343'].AT.av).toFixed(2);
+weatherCompare.addDifferenceData = function(marsResult, cityResult) {
+  const cityAvgTemperature = (cityResult[0].main.temp - 273.15).toFixed(2);
+  const marsAvgTemperature = marsResult[0]["343"].AT.av.toFixed(2);
 
-    const averageTempDifference = (cityAvgTemperature - marsAvgTemperature);
-    console.log(averageTempDifference);
+  const averageTempDifference = cityAvgTemperature - marsAvgTemperature;
+  console.log(averageTempDifference);
 
-    $(`li.averageTempDifference`).html(-Math.abs(averageTempDifference));
-}
+  $(`li.averageTempDifference`).html(-Math.abs(averageTempDifference));
+};
 
-weatherCompare.getUserCity = function(){
-    $('.cityWeather').on("submit", function(event){
-        event.preventDefault();
-        userCity = $('.cityWeather input').val();
+weatherCompare.getUserCity = function() {
+  $(".cityWeather").on("submit", function(event) {
+    event.preventDefault();
+    userCity = $(".cityWeather input").val();
 
-        if (userCity !== ''){
-            const userCityFunction = weatherCompare.getCityWeather(userCity);
-            const marsWeatherFunction = weatherCompare.getMarsWeather();
+    if (userCity !== "") {
+      const userCityFunction = weatherCompare.getCityWeather(userCity);
+      const marsWeatherFunction = weatherCompare.getMarsWeather();
 
-            // console.log(userCityFunction, marsWeatherFunction);
+      // console.log(userCityFunction, marsWeatherFunction);
 
-            $.when(userCityFunction, marsWeatherFunction).done(function(cityData, marsData){
-
-                weatherCompare.addMarsData(marsData);
-                weatherCompare.addCityData(cityData);
-                weatherCompare.addDifferenceData(marsData, cityData);
-
-            })
-        } else {
-            alert("PLZ ENTER A CITY!!!!");
-        }
-    });
-}
+      $.when(userCityFunction, marsWeatherFunction).done(function(
+        cityData,
+        marsData
+      ) {
+        weatherCompare.addMarsData(marsData);
+        weatherCompare.addCityData(cityData);
+        weatherCompare.addDifferenceData(marsData, cityData);
+      });
+    } else {
+      alert("PLZ ENTER A CITY!!!!");
+    }
+  });
+};
 
 // Start app
 weatherCompare.init = function() {
-    weatherCompare.getUserCity();
-}
+  weatherCompare.getUserCity();
+};
 
-$(function () {
-    weatherCompare.init();
+$(function() {
+  weatherCompare.init();
 });
